3dc22140a990d2528168ecf0df15e11ebca2744d
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-09-17 22:29:28 +0530

    Create leetcode_Q202.cpp

---
+++ leetcode_Q202.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        while (n !=1 && n!=4 ) {
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            n = sum;
+        }
+        
+        return n == 1;
+    }
+};
