afe28098f1d729e242f30e8c438077ae54c964dc
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-10-21 00:22:01 +0530

    Create leetcode_Q412.cpp

---
+++ leetcode_Q412.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> r;
+        int i=1;
+        for(i=1;i<=n;++i)
+        {
+            if(i%3==0&&i%5==0)
+            r.push_back("FizzBuzz");
+            else if(i%5==0)
+            r.push_back("Buzz");
+            else if(i%3==0)
+            r.push_back("Fizz");
+            else
+            r.push_back(to_string(i));
+        }
+        return r;
+    }
+};
