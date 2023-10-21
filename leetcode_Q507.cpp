caf9db6e26066f66fc194f701e767d38b9866c73
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-10-21 14:48:46 +0530

    Update leetcode_Q507.cpp

--- leetcode_Q507.cpp
+++ leetcode_Q507.cpp
@@ -1,24 +1,17 @@
-#include <string>
-
 class Solution {
 public:
-    std::string convertToBase7(int num) {
-        if (num == 0) {
-            return "0";
-        }
-        string result = "";
-        bool isNegative = num < 0;
-        num = abs(num);
-        
-        while (num > 0) {
-            result = to_string(num % 7) + result;
-            num /= 7;
-        }
-        
-        if (isNegative) {
-            result = "-" + result;
-        }
-        
-        return result;
+    bool checkPerfectNumber(int num) {
+       int sum=1;
+       int i=1;
+       for(i=2;i*i<=num;i++)
+       {
+           if(num%i==0){
+           sum=sum+i;
+            if (i != num / i) {
+            sum += num / i;
+            }
+       } 
+       }
+       return sum==num;
     }
 };
