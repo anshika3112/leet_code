fa9fb9c58fb74092ffa51c147ede7e637043e2f5
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-11-18 23:55:57 +0530

    Create leetcode_1304_find_n_unique_integer_sum_up_to_zero.cpp

---
+++ leetcode_1304_find_n_unique_integer_sum_up_to_zero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int>result;
+       if (n % 2 != 0) {
+        result.push_back(0);
+    }
+
+    // Add pairs of positive and negative integers
+    for (int i = 1; i <= n / 2; ++i) {
+        result.push_back(i);
+        result.push_back(-i);
+    }
+
+    return result;
+    }
+};
