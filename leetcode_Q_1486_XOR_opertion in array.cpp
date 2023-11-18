c34b34c8519013001184b9459470acd6fbf2af41
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-11-19 01:19:45 +0530

    Update leetcode_Q_1486_XOR_opertion in array.cpp

--- leetcode_Q_1486_XOR_opertion in array.cpp
+++ leetcode_Q_1486_XOR_opertion in array.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-        int num[20];
-        int ans;
-        for(int i=0;i<n;i++)
-        {
-            num[i]=start+2*i;
+        int num[1000];
+        int ans = 0; 
+        for (int i = 0; i < n; i++) {
+            num[i] = start + 2 * i;
         }
-        for(int i=0;i<n;i++)
-        {
-            ans=ans^num[i];
+        for (int i = 0; i < n; i++) {
+            ans = ans ^ num[i];
         }
-        return ans;
+
+        return ans; 
     }
 };
