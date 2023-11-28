d407175692b78842480e607206d7281cb2c92e42
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-11-28 21:17:08 +0530

    Create leetcode_Q_204_countPrime.cpp

---
+++ leetcode_Q_204_countPrime.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPrime(int n) {
+        if(n<=1)
+        return false;
+         for(int i=2;i<n;i++)
+        {
+            if(n%i==0)
+            return false;
+        }
+         return true;
+    }
+   int countPrimes(int n) {
+    int c=0;
+    for(int i=1;i<n;i++)
+    {
+        if(isPrime(i))
+        {
+        c++;
+        }
+    }
+    return c;
+   }
+    
+};
