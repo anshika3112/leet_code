dd3d17b3dc6408f91693024b7642cfe2b499961d
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-11-28 23:05:51 +0530

    Update leetcode_Q_204_countPrime.cpp

--- leetcode_Q_204_countPrime.cpp
+++ leetcode_Q_204_countPrime.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    bool isPrime(int n) {
-        if(n<=1)
-        return false;
-         for(int i=2;i<n;i++)
+    int countPrimes(int n) {
+        if(n==0)
+        return 0;
+        vector<bool>prime(n,true);
+        prime[0]=prime[1]=false;
+        int ans=0;
+        for(int i=2;i<n;i++)
         {
-            if(n%i==0)
-            return false;
+            if(prime[i])
+            {
+                ans++;
+                int j=2*i;
+                while(j<n)
+                {
+                    prime[j]=false;
+                    j+=i;
+                }
+            }
         }
-         return true;
+        return ans;
     }
-   int countPrimes(int n) {
-    int c=0;
-    for(int i=1;i<n;i++)
-    {
-        if(isPrime(i))
-        {
-        c++;
-        }
-    }
-    return c;
-   }
-    
 };
