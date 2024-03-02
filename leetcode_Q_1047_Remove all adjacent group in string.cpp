27f43efe182692516524f353060bbbef51cbfca4
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-03-02 22:37:07 +0530

    Create leetcode_Q_1047_Remove all adjacent group in string.cpp

---
+++ leetcode_Q_1047_Remove all adjacent group in string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int index=0;
+        string ans="";
+        while(index < s.length())
+        {
+             if( ans.length()>0  && ans[ans.length()-1]==s[index])
+             ans.pop_back();
+             else
+             {
+                 ans.push_back(s[index]);
+             }
+             index++;
+        }
+        return ans;
+    }
+};
