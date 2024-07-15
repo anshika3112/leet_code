4d1d30343a4809e283f17517bd09579d73acb9ff
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-15 23:17:23 +0530

    Create leetcode_O73 ste the matrix to zeros.cpp

---
+++ leetcode_O73 ste the matrix to zeros.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<int> row(rows, 0);
+        vector<int> col(cols, 0);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
