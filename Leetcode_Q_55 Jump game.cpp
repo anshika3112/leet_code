46bc07c9911a2277169444ac529e44f3115f171c
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-30 22:24:28 +0530

    Create Leetcode_Q_55 Jump game.cpp

---
+++ Leetcode_Q_55 Jump game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int maxReach = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = max(maxReach, i + nums[i]);
+            if (maxReach >= n - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
